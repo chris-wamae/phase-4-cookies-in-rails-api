4fe3e7a0116370771d14302675e01d028b12ce34
chris-wamae <chriswamae123@gmail.com>
2023-03-20 12:38:24 +0300

    Completed Assignment

--- config/application.rb
+++ config/application.rb
@@ -38,8 +38,8 @@ module Phase4CookiesInRailsApi
     config.api_only = true
 
     # Adding back cookies and session middleware
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore
+  config.middleware.use ActionDispatch::Cookies
+  config.middleware.use ActionDispatch::Session::CookieStore
 
     # Use SameSite=Strict for all cookies to help protect against CSRF
     config.action_dispatch.cookies_same_site_protection = :strict
